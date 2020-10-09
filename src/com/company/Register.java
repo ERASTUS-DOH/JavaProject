50725f5aad3de6678a66e34f92ee25adebe4cd0f
amo95 <jameaamo@gmail.com>
2020-10-09 15:57:11 +0000

    created getters and setters in class register

---
+++ src/com/company/Register.java
@@ -0,0 +1,22 @@
+package com.comp
+
+public class Register
+{
+    private List<Students> stdnts = new ArrayList<>();
+
+    public Register(List<Students> stdnts) {
+        this.stdnts = stdnts;
+    }
+
+    public List<Students> getStdnts() {
+        return stdnts;
+    }
+
+    public void setStdnts(List<Students> stdnts) {
+        this.stdnts = stdnts;
+    }
+
+    public List<Students> getRegister(){
+        public List<Student> reg = new ArrayList<>();
+    }
+}
